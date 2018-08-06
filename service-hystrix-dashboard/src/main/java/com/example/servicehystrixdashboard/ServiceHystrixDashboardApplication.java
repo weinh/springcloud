b5c6f939a79c4a29f70ffac5b179720cc04614bf
weinh <weinh@hundsun.com>
2018-08-06 14:08:50 +0800

    聚合监控

---
+++ service-hystrix-dashboard/src/main/java/com/example/servicehystrixdashboard/ServiceHystrixDashboardApplication.java
@@ -0,0 +1,17 @@
+package com.example.servicehystrixdashboard;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.netflix.feign.EnableFeignClients;
+import org.springframework.cloud.netflix.hystrix.EnableHystrix;
+import org.springframework.cloud.netflix.hystrix.dashboard.EnableHystrixDashboard;
+
+@SpringBootApplication
+@EnableHystrixDashboard
+public class ServiceHystrixDashboardApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ServiceHystrixDashboardApplication.class, args);
+    }
+}
